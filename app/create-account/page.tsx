8abafddb18a9d2076803b6faed39a66d7df87b2b
김종진(SugarSyrup) <tlfvm04@naver.com>
2025-06-13 16:50:11 +0900

    zod

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,31 +1,51 @@
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+"use client";
+
+import { useActionState } from "react";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
+import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
+
+import { createAccount } from "./actions";
 
 export default function CreateAccount() {
+  const [state, dispatch] = useActionState(createAccount, null);
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="text" placeholder="Username" required errors={[]} />
-        Add commentMore actions
-        <FormInput type="email" placeholder="Email" required errors={[]} />
-        <FormInput
+      <form className="flex flex-col gap-3" action={dispatch}>
+        <Input
+          type="text"
+          name="username"
+          placeholder="Username"
+          minLength={3}
+          maxLength={10}
+          required
+          errors={state?.fieldErrors.username}
+        />
+        <Input type="email" name="email" placeholder="Email" required />
+        <Input
           type="password"
+          name="password"
           placeholder="Password"
+          minLength={PASSWORD_MIN_LENGTH}
           required
-          errors={[]}
+          errors={state?.fieldErrors.password}
         />
-        <FormInput
+        <Input
           type="password"
+          name="confirm_password"
           placeholder="Confirm Password"
+          minLength={PASSWORD_MIN_LENGTH}
           required
-          errors={[]}
+          errors={state?.fieldErrors.confirm_password}
         />
-        <FormButton loading={false} text="Create account" />
+        <Button text="Create account" />
       </form>
       <SocialLogin />
     </div>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,40 @@
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+"use client";
+
+import { useFormState } from "react-dom";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
+import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
+
+import { logIn } from "./actions";
 
 export default function LogIn() {
+  const [state, dispatch] = useFormState(logIn, null);
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
-        <FormInput
+      <form action={dispatch} className="flex flex-col gap-3">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
+        <Input
+          name="password"
           type="password"
           placeholder="Password"
           required
-          errors={[]}
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
         />
-        <FormButton loading={false} text="Log in" />
+        <Button text="Log in" />
       </form>
       <SocialLogin />
     </div>
